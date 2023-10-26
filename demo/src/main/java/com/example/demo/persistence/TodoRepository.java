2c6529a62eee096fd16701506a434d697205f663
macho199 <macho199@gmail.com>
2023-10-26 21:19:22 +0900

    add Creation Query

--- demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -1,11 +1,17 @@
 package com.example.demo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.TodoEntity;
+import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
-    
+    List<TodoEntity> findByUserId(String userId);
+
+    // ?1 은 메서드의 매개변수의 순서 위치이다.
+    // @Query("select * from TodoEntity t where t.userId = ?1")
+    // List<TodoEntity> findByUserIdQuery(String userId);
 }
