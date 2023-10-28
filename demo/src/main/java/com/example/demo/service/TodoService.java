4325a25f420907c4b37237075909acc5c1977851
macho199 <macho199@gmail.com>
2023-10-28 14:43:29 +0900

    add PUT, POST method

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,42 @@ public class TodoService {
         log.info("Entity id : {} is saved.", entity.getId());
 
         return repository.findByUserId(entity.getUserId());
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        } catch (Exception e) {
+            log.error("error deleting entity", entity.getId(), e);
+
+            throw new RuntimeException("error deleting entity" + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+    }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
+
+        // (2) 넘겨받은 엔티티 id를 이용해 TodoEntity를 가져온다. 존재하지 않는 엔티티는 업데이트할 수 없기 때문이다.
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent(todo -> {
+            // (3) 반환된 TodoEntity가 존재하면 값을 새 entity의 값으로 덮어 씌운다.
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            // (4) 데이터베이스에 새 값을 저장한다.
+            repository.save(todo);
+        });
+
+        return retrieve(entity.getUserId());
     }
 
     private void validate(final TodoEntity entity) {
