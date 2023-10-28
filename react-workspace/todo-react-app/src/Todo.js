84c3ba6f5d3edc4ac321258f5941e9d8320905e6
macho199 <macho199@gmail.com>
2023-10-28 18:08:23 +0900

    react add Material ui

--- react-workspace/todo-react-app/src/Todo.js
+++ react-workspace/todo-react-app/src/Todo.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
+import { ListItem, ListItemText, InputBase, Checkbox } from "@mui/material";
 
 const Todo = (props) => {
-    const [item, setItem] = useState(props.item);
+	const [item, setItem] = useState(props.item);
 
-    return (
-        <div className="Todo">
-            <input 
-                type="checkbox"
-                id={item.id}
-                name={item.id}
-                checked={item.done}
-            />
-            <label id={item.id}>{item.title}</label>
-        </div>
-    );
+	return (
+		<ListItem>
+			<Checkbox checked={item.done} />
+			<ListItemText>
+				<InputBase 
+					inputProps={{ "aria-label": "naked" }}
+					type="text"
+					id={item.id}
+					name={item.id}
+					value={item.title}
+					multiline={true}
+					fullWidth={true}
+				/>
+			</ListItemText>
+		</ListItem>
+	);
 };
 
 export default Todo;
